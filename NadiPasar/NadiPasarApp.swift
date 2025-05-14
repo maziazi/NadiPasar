83c29a5e32adc51b0241a6d55c13f759c507c5dd
maziazi <mazis9651@gmail.com>
2025-05-15 02:14:27 +0700

    Initial Commit

---
+++ NadiPasar/NadiPasarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NadiPasarApp.swift
+//  NadiPasar
+//
+//  Created by Muhamad Azis on 15/05/25.
+//
+
+import SwiftUI
+
+@main
+struct NadiPasarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NadiPasarTests/NadiPasarTests.swift
@@ -0,0 +1,17 @@
+//
+//  NadiPasarTests.swift
+//  NadiPasarTests
+//
+//  Created by Muhamad Azis on 15/05/25.
+//
+
+import Testing
+@testable import NadiPasar
+
+struct NadiPasarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
